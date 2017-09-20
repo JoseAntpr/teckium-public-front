2bd93cb8724eecc6e44810acc8457669d8099ec0
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2017-09-20 23:41:06 +0200

    Added other html snippet

--- src/blogs/views.py
+++ src/blogs/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "blogs/index.html")
+    return render(request, "blogs/list.html")
+
+
+def detail(request):
+    return render(request, "blogs/detail.html")
--- src/teckiumDjangoFront/urls.py
+++ src/teckiumDjangoFront/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from blogs.views import index
+from blogs.views import index, detail
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name="index"),
+    url(r'^detail', detail, name="blog-detail"),
 ]
