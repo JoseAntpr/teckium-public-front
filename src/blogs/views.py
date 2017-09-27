56b88ad192b5b40e542c7ea73f0e51e04a8ef57a
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2017-09-27 17:07:58 +0200

    request to post api added

--- src/blogs/views.py
+++ src/blogs/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    blogs = requests.get("http://127.0.0.1:8001/api/1.0/blogs/")
+    posts = requests.get("http://127.0.0.1:8001/api/1.0/posts/")
 
     context = {
-        'blogs': blogs.json()['results']
+        'posts': posts.json()['results']
     }
     return render(request, "blogs/index.html", context)
 
